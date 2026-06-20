1419a793633411b2de292d0f73415ccbb9c627ca
Airadale Tarala <sj2.atarala26@joysistvi.edu.ph>
2026-06-20 15:30:12 +0800

    Add discussion code for day2 and day3

---
+++ src/day2/discussion/SampleCode.java
@@ -0,0 +1,19 @@
+
+package day2.discussion; // package
+
+
+public class SampleCode { //class
+    public static void main(String[] args) { // main method
+        System.out.println("Hello Classmates"); // print statement
+    }
+    
+}
+
+// Main Method (psvm) -> starting point of programs execution
+// Print Statement  (sout) -> it tells the computer to print something
+
+/*
+    2 Types of Comments
+    1. Single Line Comments
+    2. Multi Line Comments
+*/
---
+++ src/day3/discussion/SampleCode.java
@@ -0,0 +1,22 @@
+
+package day3.discussion; // package
+
+
+
+public class SampleCode { //class
+    public static void main(String[] args) { // main method
+        System.out.println("Hello Classmates"); // print statement
+    }
+    
+}
+
+// Syntax -> structure of code
+// Syntax Rule -> Syntax Error
+// Main Method (psvm) -> starting point of programs execution
+// Print Statement  (sout + tab -> it tells the computer to print something
+
+/*
+    2 Types of Comments
+    1. Single Line Comments
+    2. Multi Line Comments
+*/
