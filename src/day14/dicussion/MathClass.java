6956fa2c079c9dbcf677c48a8686f7fc2a326f1c
Airadale Tarala <sj2.atarala26@joysistvi.edu.ph>
2026-07-06 19:05:46 +0800

    Add Activity for day 14

--- src/day14/dicussion/MathClass.java
+++ src/day14/dicussion/MathClass.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package day14.dicussion;
 
-/**
- *
- * @author UNIMAX
- */
 public class MathClass {
-    
+
+    public static void main(String[] args) {
+        
+    }
 }
